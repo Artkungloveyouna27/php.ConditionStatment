7308d397fd7c5694c819cc3a65c065107c5d15b4
Artkungloveyouna27 <66040233104@udru.ac.th>
2024-11-19 09:59:02 +0700

    update

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>การใช้าคำสั่งแบบ Condition (กลุ่มคำสั่ง if)</h1>
+    <?php
+        echo "<h2>คำสั่ง if </h2>";
+        $x = 5;
+        $y = 8;
+        echo "\$x = $x";
+        echo "\$x = $y";
+        if($x<$y) {
+            echo "ค่าในตัวแปร ป น้อยกว่าค่าในตัวแปร y <br>";
+
+        }
+        else{
+            echo "ค่าในตัวแปร y น้อยกว่าค่าในตัวแปร x <br>";
+        }
+        echo "----------------------------------------------------------";
+    ?>    
+
+</body>
+</html>
