76316e6080750b8bcf9f2657d45b4abbd753abe9
Artkungloveyouna27 <66040233104@udru.ac.th>
2024-11-26 10:31:51 +0700

    update

---
+++ demo.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ทดสอบคำส่งค่าเป็น method GET</title>
+</head>
+<body>
+    <h2>ทดสอบคำส่งค่าเป็น method GET (ผ่าน URL) </h2>
+    <a href="test_get.php?subject=PHP&web=www.google.com">Click ที่นี่</a>
+    
+</body>
+</html>
