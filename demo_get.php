5e7d215964c367dcc0a90d8f8d21ea729a6aede6
Artkungloveyouna27 <66040233104@udru.ac.th>
2024-11-26 10:42:19 +0700

    update

--- demo_get.php
+++ demo_get.php
@@ -3,9 +3,16 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>ให้กรอกข้อมูลลงไปในฟอร๋อม</title>
 </head>
 <body>
-    
+    <h2>โปรดกรอกข้อมูลลงไปในฟอร์ม</h2>
+    <form action="test_get.php" method = "get"> <br>
+        ชื่อวิชา : <input type="text" name="subject"> <br>
+        เว็บไซต์ที่แนะนำ : <input type="text" name="web"> <br>
+        <input type="summit">
+    </form>
+    <br>
+    <a href="test_get.php?subject=PHP&web=www.google.com">Click ที่นี่</a>
 </body>
 </html>
